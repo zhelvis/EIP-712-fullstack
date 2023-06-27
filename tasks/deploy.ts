4fec4dffa7da33f15bfad90a47d798e16afb4a06
Vladimir Zhelvis <v.zhelvis@adguard.com>
2023-06-27 03:09:45 +0300

    fix deploy task

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -3,6 +3,7 @@ import { task } from 'hardhat/config';
 task('deploy', 'Deploy example contract').setAction(async (_, { ethers }) => {
     const Messenger = await ethers.getContractFactory('Messenger');
     const messenger = await Messenger.deploy();
-    await messenger.deployed();
-    console.log('Contract address: ', messenger.address);
+    await messenger.waitForDeployment();
+    const address = await messenger.getAddress();
+    console.log('Contract address: ', address);
 });
